3834c67bb60913411490f3cb8ce246ba44ee6748
Dylan Cerenov <cerenov.d@northeastern.edu>
2025-02-02 15:23:06 -0500

    Adding responsive sidebar clicking

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,111 @@
+import "./index.css";
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lectus
+        tellus, iaculis sit amet tincidunt ut, finibus et nisl. Fusce erat
+        lectus, ornare malesuada lorem nec, pretium scelerisque sem. Proin a
+        arcu ac lacus finibus mollis. Nunc rhoncus, erat at eleifend
+        consectetur, odio lectus dignissim massa, at porttitor augue lorem eu
+        sem. Donec lacus dolor, tincidunt quis mattis imperdiet, auctor quis
+        nisl. Sed et massa est. Maecenas mollis, dolor ac eleifend pellentesque,
+        velit libero rhoncus sapien, sit amet pulvinar ex lacus at dui. Nulla
+        non sollicitudin mauris. Aliquam viverra efficitur metus id tristique.
+        Phasellus gravida est sit amet lectus gravida, sed dictum mi
+        pellentesque. Pellentesque malesuada metus vel mauris hendrerit, eget
+        porta nibh varius. Phasellus purus nisl, fringilla et est at, blandit
+        auctor est. Etiam massa justo, dignissim malesuada odio sed, interdum
+        ullamcorper lectus. Nam a massa placerat, dictum leo vel, fermentum
+        velit. In imperdiet dapibus libero ut suscipit. Maecenas viverra varius
+        risus vel vehicula. Curabitur mi eros, facilisis a libero quis, sodales
+        pulvinar nunc.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lectus
+        tellus, iaculis sit amet tincidunt ut, finibus et nisl. Fusce erat
+        lectus, ornare malesuada lorem nec, pretium scelerisque sem. Proin a
+        arcu ac lacus finibus mollis. Nunc rhoncus, erat at eleifend
+        consectetur, odio lectus dignissim massa, at porttitor augue lorem eu
+        sem. Donec lacus dolor, tincidunt quis mattis imperdiet, auctor quis
+        nisl. Sed et massa est. Maecenas mollis, dolor ac eleifend pellentesque,
+        velit libero rhoncus sapien, sit amet pulvinar ex lacus at dui. Nulla
+        non sollicitudin mauris. Aliquam viverra efficitur metus id tristique.
+        Phasellus gravida est sit amet lectus gravida, sed dictum mi
+        pellentesque. Pellentesque malesuada metus vel mauris hendrerit, eget
+        porta nibh varius. Phasellus purus nisl, fringilla et est at, blandit
+        auctor est. Etiam massa justo, dignissim malesuada odio sed, interdum
+        ullamcorper lectus. Nam a massa placerat, dictum leo vel, fermentum
+        velit. In imperdiet dapibus libero ut suscipit. Maecenas viverra varius
+        risus vel vehicula. Curabitur mi eros, facilisis a libero quis, sodales
+        pulvinar nunc.
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lectus
+        tellus, iaculis sit amet tincidunt ut, finibus et nisl. Fusce erat
+        lectus, ornare malesuada lorem nec, pretium scelerisque sem. Proin a
+        arcu ac lacus finibus mollis. Nunc rhoncus, erat at eleifend
+        consectetur, odio lectus dignissim massa, at porttitor augue lorem eu
+        sem. Donec lacus dolor, tincidunt quis mattis imperdiet, auctor quis
+        nisl. Sed et massa est. Maecenas mollis, dolor ac eleifend pellentesque,
+        velit libero rhoncus sapien, sit amet pulvinar ex lacus at dui. Nulla
+        non sollicitudin mauris. Aliquam viverra efficitur metus id tristique.
+        Phasellus gravida est sit amet lectus gravida, sed dictum mi
+        pellentesque. Pellentesque malesuada metus vel mauris hendrerit, eget
+        porta nibh varius. Phasellus purus nisl, fringilla et est at, blandit
+        auctor est. Etiam massa justo, dignissim malesuada odio sed, interdum
+        ullamcorper lectus. Nam a massa placerat, dictum leo vel, fermentum
+        velit. In imperdiet dapibus libero ut suscipit. Maecenas viverra varius
+        risus vel vehicula. Curabitur mi eros, facilisis a libero quis, sodales
+        pulvinar nunc.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lectus
+        tellus, iaculis sit amet tincidunt ut, finibus et nisl. Fusce erat
+        lectus, ornare malesuada lorem nec, pretium scelerisque sem. Proin a
+        arcu ac lacus finibus mollis. Nunc rhoncus, erat at eleifend
+        consectetur, odio lectus dignissim massa, at porttitor augue lorem eu
+        sem. Donec lacus dolor, tincidunt quis mattis imperdiet, auctor quis
+        nisl. Sed et massa est. Maecenas mollis, dolor ac eleifend pellentesque,
+        velit libero rhoncus sapien, sit amet pulvinar ex lacus at dui. Nulla
+        non sollicitudin mauris. Aliquam viverra efficitur metus id tristique.
+        Phasellus gravida est sit amet lectus gravida, sed dictum mi
+        pellentesque. Pellentesque malesuada metus vel mauris hendrerit, eget
+        porta nibh varius. Phasellus purus nisl, fringilla et est at, blandit
+        auctor est. Etiam massa justo, dignissim malesuada odio sed, interdum
+        ullamcorper lectus. Nam a massa placerat, dictum leo vel, fermentum
+        velit. In imperdiet dapibus libero ut suscipit. Maecenas viverra varius
+        risus vel vehicula. Curabitur mi eros, facilisis a libero quis, sodales
+        pulvinar nunc.
+        <div className="wd-float-done"></div>
+      </div>
+      <div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+          Yellow{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+          Blue{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+          Red{" "}
+        </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <div className="wd-float-done"></div>
+      </div>
+    </div>
+  );
+}
