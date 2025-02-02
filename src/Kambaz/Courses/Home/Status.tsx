4eea332f0261a5a5eb28f3f48dac04f6eb3a5e56
Dylan Cerenov <cerenov.d@northeastern.edu>
2025-02-02 17:07:12 -0500

    need to do assignment screen

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -1,37 +1,111 @@
+import { Button } from "react-bootstrap";
+import { LuDownload } from "react-icons/lu";
+import { CiLogout } from "react-icons/ci";
+import { FaBell, FaCheckCircle } from "react-icons/fa";
+import { IoMdHome } from "react-icons/io";
+import { MdDoNotDisturbAlt, MdOutlineBarChart } from "react-icons/md";
+import { TfiAnnouncement } from "react-icons/tfi";
+
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
+    <div className="text-nowrap">
       <h2>Course Status</h2>
-      <div>
-        <button>Unpublish</button> <button>Publish</button>
-      </div>
-      <p></p>
-      <div>
-        <button>Import Existing Content</button>
-      </div>
-
-      <div>
-        <button>Import from Commons</button>
-      </div>
-
-      <div>
-        <button>Choose Home Page</button>
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <Button variant="secondary" size="lg" className="w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish{" "}
+          </Button>{" "}
+        </div>
+        <div className="w-50">
+          <Button variant="success" size="lg" className="w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish{" "}
+          </Button>{" "}
+        </div>
       </div>
 
-      <div>
-        <button>View Course Stream</button>
+      <br />
+      <div className="pb-1">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="me-1 w-100 text-start"
+          id="wd-import-existing-content"
+        >
+          <LuDownload />
+          {" Import Existing Content"}
+        </Button>
       </div>
 
-      <div>
-        <button>New Announcement</button>
+      <div className="pb-1">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="me-1 w-100 text-start"
+          id="wd-import-from-commons"
+        >
+          <CiLogout />
+          {" Import from Commons"}
+        </Button>
       </div>
 
-      <div>
-        <button>New Analytics</button>
+      <div className="pb-1">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="me-1 w-100 text-start"
+          id="wd-choose-home-page"
+        >
+          <IoMdHome />
+          {" Choose Home Page"}
+        </Button>
       </div>
 
-      <div>
-        <button>View Course Notifications</button>
+      <div className="pb-1">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="me-1 w-100 text-start"
+          id="wd-view-course-stream"
+        >
+          <MdOutlineBarChart />
+          {" View Course Stream"}
+        </Button>
+      </div>
+
+      <div className="pb-1">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="me-1 w-100 text-start"
+          id="wd-new-announcement"
+        >
+          <TfiAnnouncement />
+          {" New Announcement"}
+        </Button>
+      </div>
+
+      <div className="pb-1">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="me-1 w-100 text-start"
+          id="wd-new-analytics"
+        >
+          <MdOutlineBarChart />
+          {" New Analytics"}
+        </Button>
+      </div>
+
+      <div className="pb-1">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="me-1 w-100 text-start"
+          id="wd-view-course-notifications"
+        >
+          <FaBell />
+          {" View Course Notifications"}
+        </Button>
       </div>
     </div>
   );
--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -3,7 +3,6 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import "./styles.css";
 
 export default function KambazNavigation() {
   const location = useLocation();
