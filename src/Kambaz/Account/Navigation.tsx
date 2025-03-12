e1fd909b2dd5061a2121cff9db437c09193c0ed6
Dylan Cerenov <cerenov.d@northeastern.edu>
2025-03-12 01:22:23 -0400

    4.4.5.1

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,34 +1,24 @@
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const location = useLocation();
 
   return (
     <div style={{ paddingRight: "20px" }}>
       <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-        <Link
-          to="/Kambaz/Account/Signin"
-          className={`list-group-item border border-0 ${
-            location.pathname.includes("/Signin") ? "active" : "text-danger"
-          }`}
-        >
-          Signin
-        </Link>
-        <Link
-          to="/Kambaz/Account/Signup"
-          className={`list-group-item border border-0 ${
-            location.pathname.includes("/Signup") ? "active" : "text-danger"
-          }`}
-        >
-          Signup
-        </Link>
-        <Link
-          to="/Kambaz/Account/Profile"
-          className={`list-group-item border border-0 ${
-            location.pathname.includes("/Profile") ? "active" : "text-danger"
-          }`}
-        >
-          Profile
-        </Link>
+        {links.map((link) => (
+          <Link
+            to={`/Kambaz/Account/${link}`}
+            className={`list-group-item border border-0 ${
+              location.pathname.includes(link) ? "active" : "text-danger"
+            }`}
+          >
+            {link}
+          </Link>
+        ))}
       </div>
     </div>
   );
