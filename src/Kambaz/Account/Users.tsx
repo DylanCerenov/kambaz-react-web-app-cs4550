958ffa321a26db668599e90b3cfc029c155972b4
Dylan Cerenov <cerenov.d@northeastern.edu>
2025-04-11 16:53:53 -0400

    Completed

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -7,9 +7,7 @@ import { FaPlus } from "react-icons/fa";
 export default function Users() {
   const [users, setUsers] = useState<any[]>([]);
   const [role, setRole] = useState("");
-  const [name, setName] = useState("");
   const filterUsersByName = async (name: string) => {
-    setName(name);
     if (name) {
       const users = await client.findUsersByPartialName(name);
       setUsers(users);
